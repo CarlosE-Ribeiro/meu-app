bbe2316baba3cce1f42d7f5bf49748f69d401ebd
Carlos E-Ribeiro <carlosesilva466@gmail.com>
2025-10-02 23:52:10 -0300

    atualizando arquivos

--- src/main/java/com/example/app/app.java
+++ src/main/java/com/example/app/app.java
@@ -1,1 +1,7 @@
-﻿(cole aqui o conteúdo do App.java)
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Olá Jenkins! Build funcionando. ✅");
+    }
+}
